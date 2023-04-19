772665294f60cc707ec393cc5f1adbeacf775c14
killearnrui <kangrui2002@yahoo.com>
2023-04-19 09:47:11 -0400

    Update hooks.py

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -1,16 +1,15 @@
 _callbacks= {}
 
-def register(hook, 0):
-    register_callback(func):
-        _callbacks.setdefault(hook, {})
-        _callbacks.setdefault(order, [])
-        _callbacks.append(func)
+def register(hook, order=0):
+    def register_callback(func):
+        _callbacks.setdefault(hook, {}).setdefault(order, []).append(func)
         return func
-    return register.register_callback
+    return register_callback
     
 def event(hook, *args):
-    for order in sorted(_callbacks.get(hook, {}))
-    func(*args)
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            func(*args)
 
 def filter(hook, value, *args):
     for order in sorted(_callbacks.get(hook, {})):
